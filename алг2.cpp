f2baa0c62f44d874516bf7df73aa1b66be898bf0
what-a-draaaag <113132993+what-a-draaaag@users.noreply.github.com>
2022-11-03 01:57:57 +0300

    Add files via upload

---
+++ алг2.cpp
@@ -0,0 +1,23 @@
+﻿#include <cassert>
+#include <iostream>
+#include <map>
+using namespace std;
+int gcd(int a, int b) {
+	if (a == 0) return b;
+	return gcd(b % a, a);
+}
+
+int gcdExt(int a, int b, int& x, int& y,int oa,int ob) {
+	if (a == 0) { x = 0; y = 1; return b; }
+	auto res = gcdExt(b % a, a, y, x,oa,ob);
+	x -= (b / a) * y;
+	if (x*oa+y*ob==gcd(oa,ob))
+		cout << x << " " << y << endl;
+	return res;
+}
+int main() {
+	int a, b, x, y;
+	cin >> a >> b;
+	gcdExt(a, b, x, y,a,b);
+	return 0;
+}
